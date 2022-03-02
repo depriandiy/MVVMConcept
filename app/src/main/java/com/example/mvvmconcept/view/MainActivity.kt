0b5783e2fe0d1f1ee8f301191a15c718f989533f
depriandiy <depriandianton22@gmail.com>
2022-03-02 12:30:12 +0700

    Add view

--- app/src/main/java/com/example/mvvmconcept/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmconcept/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvmconcept
+package com.example.mvvmconcept.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmconcept.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
