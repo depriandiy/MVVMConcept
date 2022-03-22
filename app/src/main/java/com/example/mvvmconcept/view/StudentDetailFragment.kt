2039eabac1e39d40347c2a0144536058e8fcdf24
depriandiy <depriandianton22@gmail.com>
2022-03-22 23:35:25 +0700

    Homework W6

--- app/src/main/java/com/example/mvvmconcept/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/mvvmconcept/view/StudentDetailFragment.kt
@@ -8,13 +8,13 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmconcept.R
 import com.example.mvvmconcept.model.Student
+import com.example.mvvmconcept.util.loadImage
 import com.example.mvvmconcept.viewmodel.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_student_detail.*
 import kotlinx.android.synthetic.main.student_list_item.*
 
 class StudentDetailFragment : Fragment() {
     private lateinit var viewModel: DetailViewModel
-    private lateinit var detailStudent: Student
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,25 +25,27 @@ class StudentDetailFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var studentId : String? = "detail"
+        arguments?.let {
+            studentId = StudentDetailFragmentArgs.fromBundle(requireArguments()).studentID
+        }
+
         viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
-        viewModel.fetch()
+        viewModel.fetch(studentId)
 
         observeViewModel()
     }
 
     private fun observeViewModel() {
         viewModel.studentsLD.observe(viewLifecycleOwner) {
-            detailStudent = Student(
-                viewModel.studentsLD.value?.id,
-                viewModel.studentsLD.value?.name,
-                viewModel.studentsLD.value?.dob,
-                viewModel.studentsLD.value?.phone,
-                viewModel.studentsLD.value?.photoURL
-            )
-            editID.setText(detailStudent.id)
-            editName.setText(detailStudent.name)
-            editDOB.setText(detailStudent.dob)
-            editPhone.setText(detailStudent.phone)
+            val student = viewModel.studentsLD.value
+            student?.let {
+                imgStudentDetail.loadImage(it.photoURL, progressLoadingPhotoStudentDetail)
+                editID.setText(it.id)
+                editName.setText(it.name)
+                editDOB.setText(it.dob)
+                editPhone.setText(it.phone)
+            }
         }
     }
 
